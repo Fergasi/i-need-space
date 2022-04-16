81aa7ab3c33e442810f2c7dec03179107b66ef67
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-04-15 22:33:43 -0400

    "first push"

---
+++ main.js
@@ -0,0 +1,8 @@
+//Declare variables
+let results = document.querySelector("#results");
+let apiKey = document.querySelector("#api-key");
+let address = document.querySelector("#address");
+let satellite = document.querySelector("#norad");
+let search = document.querySelector("#search");
+let latitude = 0
+let longditude = 0
